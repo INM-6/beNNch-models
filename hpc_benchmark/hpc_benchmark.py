17ab594fe7c383374e5ef175fd0ac4e503bb2e5d
Jan Vogelsang <47158055+JanVogelsang@users.noreply.github.com>
2025-02-13 16:42:06 +0100

    Fixed issue with threaded non-presim timers

--- hpc_benchmark/hpc_benchmark.py
+++ hpc_benchmark/hpc_benchmark.py
@@ -490,17 +490,19 @@ def run_simulation():
     d.update(final_kernel_status)
 
     # Subtract timer information from presimulation period
-    timers = ['time_collocate_spike_data', 'time_communicate_spike_data', 'time_communicate_target_data', 'time_deliver_secondary_data', 'time_deliver_spike_data', 'time_gather_secondary_data', 'time_gather_spike_data', 'time_omp_synchronization_simulation', 'time_mpi_synchronization', 'time_simulate', 'time_update']
-    timers.extend([timer + '_cpu' for timer in timers])
+    presim_timers = ['time_collocate_spike_data', 'time_communicate_spike_data', 'time_communicate_target_data', 'time_deliver_secondary_data', 'time_deliver_spike_data', 'time_gather_secondary_data', 'time_gather_spike_data', 'time_omp_synchronization_simulation', 'time_mpi_synchronization', 'time_simulate', 'time_update']
+    presim_timers.extend([timer + '_cpu' for timer in presim_timers])
+    other_timers = ['time_communicate_prepare', 'time_construction_connect', 'time_construction_create', 'time_gather_target_data', 'time_omp_synchronization_construction']
+    other_timers.extend([timer + '_cpu' for timer in timers])
 
-    for timer in timers:
+    for timer in presim_timers:
         try:
             if type(d[timer]) == tuple or type(d[timer]) == list:
                 timer_array = tuple(d[timer][tid] - intermediate_kernel_status[timer][tid] for tid in range(len(d[timer])))
                 d[timer] = timer_array[0]
                 d[timer + "_max"] = max(timer_array)
                 d[timer + "_min"] = min(timer_array)
-                d[timer + "_avg"] = np.mean(timer_array)
+                d[timer + "_mean"] = np.mean(timer_array)
                 d[timer + "_all"] = timer_array
                 d[timer + '_presim'] = intermediate_kernel_status[timer][0]
                 d[timer + "_presim_max"] = max(intermediate_kernel_status[timer])
@@ -510,6 +512,19 @@ def run_simulation():
             else:
                 d[timer] -= intermediate_kernel_status[timer]
                 d[timer + '_presim'] = intermediate_kernel_status[timer]
+        except KeyError:
+            # KeyError if compiled without detailed timers, except time_simulate
+            continue
+               
+    for timer in other_timers:
+        try:
+            if type(d[timer]) == tuple or type(d[timer]) == list:
+                timer_array = d[timer]
+                d[timer] = timer_array[0]
+                d[timer + "_max"] = max(timer_array)
+                d[timer + "_min"] = min(timer_array)
+                d[timer + "_mean"] = np.mean(timer_array)
+                d[timer + "_all"] = timer_array
         except KeyError:
             # KeyError if compiled without detailed timers, except time_simulate
             continue
